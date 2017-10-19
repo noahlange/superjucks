34bb6ebd8720e39f7171ffd53a95e770ef70ab32
Noah Lange <noahrlange@gmail.com>
2017-10-19 13:25:30 -0500

    chore(export): adds additional tests for Export node.

--- packages/superjucks/src/tests/nodes/Export.ts
+++ packages/superjucks/src/tests/nodes/Export.ts
@@ -9,3 +9,14 @@ test('should parse an Export node', async t => {
     [Nodes.Export, [Nodes.Dict, [Nodes.Symbol, 'foo'], [Nodes.Symbol, 'bar']]]
   ]);
 });
+
+test('should parse an Export node', async t => {
+  const str = '{% export default foo %}';
+  t.deepEqual(p(str), [
+    Nodes.Root,
+    [
+      Nodes.Export,
+      [Nodes.Dict, [Nodes.Pair, [Nodes.Symbol, 'default'], [Nodes.Symbol, 'foo']]]
+    ]
+  ]);
+});
